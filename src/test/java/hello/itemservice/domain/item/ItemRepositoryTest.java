c5f05a5c0277665650983d7518a20a9378a0e1a3
YunByungil <dbsquddlfz@naver.com>
2023-01-06 17:32:28 +0900

    test: 상품 저장소 테스트 생성

---
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,70 @@
+package hello.itemservice.domain.item;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach // 이 에노테이션은 테스트 하나 하나 끝날 때 마다 실행이 된다.
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        // given
+        Item item = new Item("itemA", 10000, 10);
+
+        // when
+        Item saveItem = itemRepository.save(item);
+
+        // then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(saveItem);
+
+    }
+
+    @Test
+    void findAll() {
+        // given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+
+        // when
+        List<Item> result = itemRepository.findAll(); // 테스트 목적이 findAll이기 때문에 when에서 findAll 사용
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(item1, item2);
+    }
+
+    @Test
+    void updateItem() {
+        // given
+        Item item = new Item("item1", 10000, 10);
+
+        Item savedItem = itemRepository.save(item);
+        Long itemId = savedItem.getId();
+
+        // when
+        Item updateParam = new Item("item2", 20000, 20);
+        itemRepository.update(itemId, updateParam);
+
+        // then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+    }
+
+}
