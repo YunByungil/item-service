e080c4f8014f0b0a4b38840a2aa4cbc3bf0b7ce6
YunByungil <dbsquddlfz@naver.com>
2023-01-06 20:11:48 +0900

    feat: 상품 수정 (폼으로 이동) 추가

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -88,6 +88,13 @@ public class BasicItemController {
        return "basic/item";
     }
 
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
     /**
      * 테스트용 데이터 추가
      */
